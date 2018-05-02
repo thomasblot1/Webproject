abcdcffcd435c0d5d70b8b557d09fc0c0c59fd7a
SBragas <sarah.bragas@edu.ece.fr>
2018-05-02 16:33:53 +0200

    Create AdministrateurTest.php

---
+++ AdministrateurTest.php
@@ -0,0 +1,28 @@
+<html>
+
+<menu>
+<div id="menu">
+  <ul id="onglets">
+    <li class="active"><a href="Accueil.php"> Accueil </a></li>
+	<li><a href="Vous.php"> Vous </a></li>
+    <li><a href="Reseau.php"> Reseau </a></li>
+    <li><a href="Notif.php"> Notif </a></li>
+    <li><a href="Emplois.php"> Emplois </a></li>
+  	<li><a href="AdministrateurTest.php"> Gestion des utilisateurs </a></li>
+    <li><a href="Connexion.php"> Deconnexion </a></li>
+  </ul>
+</div>
+</menu>
+
+<form>
+	<h1>Administrateur - Accès</h1>
+	<h3>Nom:</h3>
+	<input type="text" name="nom">
+	<h3>Prénom:</h3>
+	<input type="text" name="prenom">
+	<h3>Mail:</h3>
+	<input type="text" name="mail">
+	<input type="button" value="Vérification">
+	<form>
+
+</html>

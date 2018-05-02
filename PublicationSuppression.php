cbbb553cef237b63dbea486374fa5675eeaeae24
thomasblot1 <36473514+thomasblot1@users.noreply.github.com>
2018-05-02 14:48:56 +0200

    Add files via upload

---
+++ PublicationSuppression.php
@@ -0,0 +1,20 @@
+<html>
+<menu>
+<div id="menu">
+  <ul id="onglets">
+    <li class="active"><a href="Accueil.php"> Accueil </a></li>
+	<li><a href="Vous.php"> Vous </a></li>
+    <li><a href="Reseau.php"> Reseau </a></li>
+    <li><a href="Notif.php"> Notif </a></li>
+    <li><a href="Emplois.php"> Emplois </a></li>
+    <li><a href="Connexion.php"> Deconnexion </a></li>
+  </ul>
+</div>
+</menu>
+<h1>Suppression d'une publication</h1>
+//Affichage de 3 colonnes: photo, video, event
+//
+//
+//
+<input type="Button" value="Confirmer la suppression">
+</html>
---
+++ Reseau.php
@@ -0,0 +1,38 @@
+<html>
+<menu>
+<div id="menu">
+  <ul id="onglets">
+    <li class="active"><a href="Accueil.php"> Accueil </a></li>
+	<li><a href="Vous.php"> Vous </a></li>
+    <li><a href="Reseau.php"> Reseau </a></li>
+    <li><a href="Notif.php"> Notif </a></li>
+    <li><a href="Emplois.php"> Emplois </a></li>
+    <li><a href="Connexion.php"> Deconnexion </a></li>
+  </ul>
+</div>
+</menu>
+
+<body>
+<h1>Mon Reseau</h1>
+<fieldset>
+<legend>NomAmi1</legend>
+
+</fieldset>
+
+<fieldset>
+<legend>NomAmi2</legend>
+
+</fieldset>
+
+<fieldset>
+<legend>NomAmi3</legend>
+
+</fieldset>
+
+<fieldset>
+<legend>NomAmi4</legend>
+
+</fieldset>
+
+</body>
+</html>

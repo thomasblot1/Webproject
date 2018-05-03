5100b0262ec7a062fabca385726eb4043b0d728c
SBragas <sarah.bragas@edu.ece.fr>
2018-05-03 10:21:27 +0200

    Implémentation du style du menu

--- Emplois.php
+++ Emplois.php
@@ -1,12 +1,13 @@
 <html>
 <menu>
+<link href="css/Emplois.css" rel="stylesheet">
 <div id="menu">
   <ul id="onglets">
-    <li class="active"><a href="Accueil.php"> Accueil </a></li>
+    <li><a href="Accueil.php"> Accueil </a></li>
 	<li><a href="Vous.php"> Vous </a></li>
     <li><a href="Reseau.php"> Reseau </a></li>
     <li><a href="Notif.php"> Notif </a></li>
-    <li><a href="Emplois.php"> Emplois </a></li>
+    <li class="active"><a href="Emplois.php"> Emplois </a></li>
     <li><a href="AdministrateurTest.php"> Gestion des utilisateurs </a><li>
     <li><a href="Connexion.php"> Deconnexion </a></li>
   </ul>

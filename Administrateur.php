6eaa972ae0e54d4cd32737c20f3aa494f1e64a0d
SBragas <sarah.bragas@edu.ece.fr>
2018-05-03 10:17:07 +0200

    Implémentation du design du menu

--- Administrateur.php
+++ Administrateur.php
@@ -1,13 +1,14 @@
 <html>
 <menu>
+<link href="css/Reseau.css" rel="stylesheet">
 <div id="menu">
   <ul id="onglets">
-    <li class="active"><a href="Accueil.php"> Accueil </a></li>
+    <li><a href="Accueil.php"> Accueil </a></li>
 	<li><a href="Vous.php"> Vous </a></li>
-    <li><a href="Reseau.php"> Reseau </a></li>
+    <li class="active"><a href="Reseau.php"> Reseau </a></li>
     <li><a href="Notif.php"> Notif </a></li>
     <li><a href="Emplois.php"> Emplois </a></li>
-	<li><a href="Administrateur.php"> Gestion des utilisateurs </a></li>
+    <li><a href="AdministrateurTest.php"> Gestion des utilisateurs </a><li>
     <li><a href="Connexion.php"> Deconnexion </a></li>
   </ul>
 </div>

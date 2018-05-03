199e680b4462160e80781d4e9efcfefc8359ebca
SBragas <sarah.bragas@edu.ece.fr>
2018-05-03 16:11:25 +0200

    Test du type d'utilisateur fait! -^0^-

--- AdministrateurTest.php
+++ AdministrateurTest.php
@@ -15,15 +15,58 @@
 </div>
 </menu>
 
-<form>
+<form method="post" action="AdministrateurTest.php">
 	<h1>Administrateur - Accès</h1>
 	<h3>Nom:</h3>
-	<input type="text" name="nom">
+	<input type="text" name="nom" id=nom />
 	<h3>Prénom:</h3>
-	<input type="text" name="prenom">
+	<input type="text" name="prenom" id=prenom />
 	<h3>Mail:</h3>
-	<input type="text" name="mail">
-	<input type="button" value="Vérification" Onclick="location.href='Administrateur.php'" >
+	<input type="text" name="mail" id=mail />
+	<input type='submit' value='Valider la sélection'>
 	<form>
 
 </html>
+
+<?php
+	$bdd="reseausocial";
+	$connect=mysqli_connect('localhost','root','');
+	$connect1=mysqli_select_db($connect,$bdd);
+	$Mail = isset($_POST['mail'])?$_POST['mail']:"";
+	$Nom = isset($_POST['nom'])?$_POST['nom']:"";
+	$Prenom = isset($_POST['prenom'])?$_POST['prenom']:"";
+
+	if(!$connect)
+	echo"pb de connection";
+else{
+	if($Mail!=""){
+		if($Prenom!=""){
+			if($Nom!=""){
+				//on construit la requete
+				$sql="SELECT Nom,Prenom,Mail FROM table_utilisateurs";
+				$res=mysqli_query($connect,$sql);
+				while($data=mysqli_fetch_assoc($res)){
+					if(($data['Nom']==$Nom) and ($data['Prenom']==$Prenom) and ($data['Mail']==$Mail)){
+						echo "Vous n'êtes pas un administrateur.";
+						header("Location:AdministrateurTest.php");
+					}
+					else{
+						$sqladmin="SELECT Nom,Prenom,Mail FROM table_admin";
+						$res1=mysqli_query($connect,$sqladmin);
+						while($data1=mysqli_fetch_assoc($res1)){
+							if(($data1['Nom']==$Nom) and ($data1['Prenom']==$Prenom) and ($data1['Mail']==$Mail)){
+								header("Location:Administrateur.php");
+							}
+						}
+					}
+				}
+				echo"Problèmes";
+			}
+			else{
+				echo"erreur nom";}
+		}
+		else{
+			echo"erreur prenom";}
+	}
+	}		
+?>

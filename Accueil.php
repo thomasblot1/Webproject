e0da98a9ac773c4d7869d04a94db5ccd53c19314
thomasblot1 <36473514+thomasblot1@users.noreply.github.com>
2018-05-03 12:06:36 +0200

    Add files via upload

--- Accueil.php
+++ Accueil.php
@@ -1,7 +1,7 @@
 <html>
 
 <menu>
-<link href="css/Accueil.css" rel="stylesheet">
+<link href="css/Reseau.css" rel="stylesheet">
 <div id="menu">
   <ul id="onglets">
     <li class="active"><a href="Accueil.php"> Accueil </a></li>
@@ -14,45 +14,35 @@
 </div>
 </menu>
 
-<h1>Accueil</h1>
-<fieldset>
-<legend>Event 1</legend>
-<input type="date" value=date1>
-//Ajout de l'icone si existante
-<h4>Description</h4>
-//Recupere la description
-<input type="button" value="Like">
-<input type="button" value="Share">
-</fieldset>
+<?php
+echo'<h1>Accueil</h1>';
+$serveur="localhost";
+$log="root";
+$compteur=1;
+$mdp="";
+$bdd="reseausocial";
+$connectique=mysqli_connect($serveur,$log,$mdp);
+$con=mysqli_select_db($connectique,$bdd);
+if(!$connectique)
+	echo"pb de connection";
+else{
+	$sq="SELECT * FROM publication ORDER BY Date DESC";
+	$resultate=mysqli_query($connectique,$sq);$compteur=1;
+while($tab=mysqli_fetch_assoc($resultate)){	
+			
+			echo'<fieldset>
+			
+		<legend>Event'.$compteur.'</legend>'.$tab['Date'].'
+		</br><img src="'.$tab['chemin'].'" alt="photo" height="500" width="500">
+		<h4>Description</h4>'.$tab['Contenu_texte'].'</br>'.'</br>'.'
+		<input type="button" value="Like">
+		<input type="button" value="Share"></br>
+		'.$tab['nb_like'].'
+		</fieldset>';$compteur=$compteur+1;
+		
+	}	
+}
 
-<fieldset>
-<legend>Event 2</legend>
-<input type="date" value=date2>
-//Ajout de l'icone si existante
-<h4>Description</h4>
-//Recupere la description
-<input type="button" value="Like">
-<input type="button" value="Share">
-</fieldset>
-
-<fieldset>
-<legend>Event 3</legend>
-<input type="date" value=date3>
-//Ajout de l'icone si existante
-<h4>Description</h4>
-//Recupere la description
-<input type="button" value="Like">
-<input type="button" value="Share">
-</fieldset>
-
-<fieldset>
-<legend>Event 4</legend>
-<input type="date" value=date4>
-//Ajout de l'icone si existante
-<h4>Description</h4>
-//Recupere la description
-<input type="button" value="Like">
-<input type="button" value="Share">
-</fieldset>
+?>
 
 </html>
--- Administrateur.php
+++ Administrateur.php
@@ -8,12 +8,12 @@
     <li class="active"><a href="Reseau.php"> Reseau </a></li>
     <li><a href="Notif.php"> Notif </a></li>
     <li><a href="Emplois.php"> Emplois </a></li>
-    <li><a href="AdministrateurTest.php"> Gestion des utilisateurs </a></li>
+    <li><a href="AdministrateurTest.php"> Gestion des utilisateurs </a><li>
     <li><a href="Connexion.php"> Deconnexion </a></li>
   </ul>
 </div>
 </menu>
 <h1>Administrateur - Gestion des utilisateurs</h1>
-<input type="Button" value="Ajout d'un utilisateur"  Onclick="location.href='AdministrateurAjout.php'">
-<input type="Button" value="Suppression d'un utilisateur" Onclick="location.href='AdministrateurSuppression.php'">
+<input type="Button" value="Ajout d'un utilisateur">
+<input type="Button" value="Suppression d'un utilisateur">
 </html>
